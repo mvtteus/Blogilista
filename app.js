b5b3c47e87b7418f78fe3920585adc117e29f053
mvtteus <matias.jarvinen@helsinki.fi>
2020-02-12 16:40:07 +0200

    aaaaa

--- app.js
+++ app.js
@@ -18,14 +18,10 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
     logger.error('error connection to MongoDB:', error.message)
   })
 
-app.use(cors())
-app.use(express.static('build'))
 app.use(express.json())
+app.use(cors())
 app.use(middleware.requestLogger)
-
 app.use('/api/blogs', blogsRouter)
-
-app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
 module.exports = app
