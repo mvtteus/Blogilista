e8defe87a6fab90c31a29a38f451d2b0b699cc34
mvtteus <matias.jarvinen@helsinki.fi>
2020-02-17 14:09:47 +0200

    4.19

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', (request, response) => {
     Blog
@@ -10,10 +19,16 @@ blogsRouter.get('/', (request, response) => {
       })
   })
   
-  blogsRouter.post('/', async (request, response, next) => {
+  blogsRouter.post('/', async (request, response) => {
     const body = request.body
+    const token = getTokenFrom(request)
   
-    const user = await User.findById(body.userId)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return response.status(401).json()
+    }
+  
+    const user = await User.findById(decodedToken.id)
   
     const blog = new Blog({
       title: body.title,
@@ -28,7 +43,7 @@ blogsRouter.get('/', (request, response) => {
     await user.save()
   
     response.json(savedBlog.toJSON())
-})
+  })
 
 blogsRouter.delete('/:id', async (request, response) =>  {
   const id = request.params.id
